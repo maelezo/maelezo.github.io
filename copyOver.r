3c58c6c931266ae7325d28995097c0dfa61ff734
dmbeskow <dnbeskow@gmail.com>
2017-04-25 22:58:40 -0400

    adjusted bulleting

--- copyOver.r
+++ copyOver.r
@@ -1,1 +1,3 @@
 system('cp -r /home/dmbeskow/Dropbox/bookdown-demo-master/_book/* /home/dmbeskow/Dropbox/maelezo.github.io')
+
+system('cp -r /home/dmbeskow/Dropbox/bookdown-demo-master/_book/* /home/dmbeskow/Dropbox/maelezo.github.io')
